88261f0d543630bf18fec36b4fb2d4620ae5e35c
tushark01 <tusharkhete118@gmail.com>
2021-07-15 16:31:29 +0530

    Day 2 completed

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "Tushar";
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
