076fdacfe6b67bfaf4c6d3fb7c0eb703fcf736bf
tushark01 <tusharkhete118@gmail.com>
2021-07-23 22:17:30 +0530

    Day-15 Completed

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -23,30 +23,45 @@ class _HomePageState extends State<HomePage> {
   }
 
   loadData() async {
+    await Future.delayed(Duration(seconds: 2));
     final catalogJson =
         await rootBundle.loadString("Assets/files/catalog_json.json");
     final decodedData = jsonDecode(catalogJson);
     var productsData = decodedData["products"];
-    print(productsData);
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+    //print(productsData);
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(40, (index) => CatalogModel.items[0]);
+    //final dummyList = List.generate(40, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: Text("iPhone eStore"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(child: Image.network(item.image)));
+                },
+                itemCount: CatalogModel.items.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
