a5de4fb1ed56ebc96210620aafec3f095db23608
tushark01 <tusharkhete118@gmail.com>
2021-07-30 16:17:11 +0530

    Day-25 Completed

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,28 +1,35 @@
 import 'package:flutter_application_1/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
-  late CatalogModel _cataog;
+  // catalog field
+  CatalogModel _catalog;
 
+  // Collection of IDs - store Ids of each item
   final List<int> _itemIds = [];
-  CatalogModel get catalog => _cataog;
+
+  // Get Catalog
+  CatalogModel get catalog => _catalog;
+
   set catalog(CatalogModel newCatalog) {
     assert(newCatalog != null);
-    _cataog = newCatalog;
+    _catalog = newCatalog;
   }
 
-  //get items in the cart:
+  // Get items in the cart
+  List<Item> get items => _itemIds.map((id) => _catalog.getById(id)).toList();
 
-  List<Item> get items => _itemIds.map((id) => _cataog.getByid(id)).toList();
-
+  // Get total price
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
-  // add item
+
+  // Add Item
 
   void add(Item item) {
     _itemIds.add(item.id);
   }
 
-  //remove item
+  // Remove Item
 
   void remove(Item item) {
     _itemIds.remove(item.id);
